2c3c6deec970bd9115d8908ba284ca879e256018
Sweekriti Satpathy <swsat@microsoft.com>
2024-07-10 10:38:12 -0700

    sample added

--- ProfilePhotoDemo/MainPage.xaml.cs
+++ ProfilePhotoDemo/MainPage.xaml.cs
@@ -1,7 +1,4 @@
-﻿using AndroidX.Lifecycle;
-using Google.Android.Material.Color.Utilities;
-using Microsoft.Maui.Graphics.Platform;
-using System;
+﻿using Microsoft.Maui.Graphics.Platform;
 using IImage = Microsoft.Maui.Graphics.IImage;
 
 namespace ProfilePhotoDemo;
@@ -11,42 +8,7 @@ public partial class MainPage : ContentPage, IDrawable
     public IImage imageDraw { get; set; }
     public MainPage()
 	{
-        usePhoto.IsVisible = false;
-        rotatePhotoClock.IsVisible = false;
-        image.IsVisible = true;
-       
-        takePhoto.Clicked += async (sender, args) =>
-        {
-            TakePhoto();
-
-            usePhoto.IsVisible = true;
-            image.IsVisible = true;
-            rotatePhotoClock.IsVisible = true;
-        };
-
-        pickPhoto.Clicked += async (sender, args) =>
-        {
-            PickPhoto();
-           
-            usePhoto.IsVisible = true;
-            rotatePhotoClock.IsVisible = true;
-            image.IsVisible = true;
-        };
-
-        rotatePhotoClock.Clicked += async (sender, args) =>
-        {
-            var service = new PlatformBitmapExportService();
-            var context = service.CreateContext(350, 350);
-            var canvas = context.Canvas;
-
-            // draw using canvas
-            var imageRotate = context.Image;
-            canvas.Rotate(90);
-            canvas.DrawImage(imageDraw, 10, 10, 350, 350);
-            Draw(canvas, new RectF(10, 10, 350, 350));
-            image.Drawable = this;
-        };
-
+        InitializeComponent();
     }
 
     public void Draw(ICanvas canvas, RectF dirtyRect)
@@ -57,14 +19,11 @@ public partial class MainPage : ContentPage, IDrawable
         }
     }
 
-
-    string base64image = "";
-    public async void TakePhoto()
+    public async void PickPhoto()
     {
         if (MediaPicker.Default.IsCaptureSupported)
         {
-            FileResult photo = await MediaPicker.Default.CapturePhotoAsync();
-
+            FileResult photo = await MediaPicker.Default.PickPhotoAsync();
             if (photo != null)
             {
                 var stream = await photo.OpenReadAsync();
@@ -80,27 +39,23 @@ public partial class MainPage : ContentPage, IDrawable
         }
     }
 
-    public async void PickPhoto()
+    void rotatePhotoClock_Clicked(System.Object sender, System.EventArgs e)
     {
-        if (MediaPicker.Default.IsCaptureSupported)
-        {
-            FileResult photo = await MediaPicker.Default.PickPhotoAsync();
+            var service = new PlatformBitmapExportService();
+            var context = service.CreateContext(350, 350);
+            var canvas = context.Canvas;
 
+            // draw using canvas
+            var imageRotate = context.Image;
+            canvas.Rotate(90);
+            // canvas.DrawImage(imageDraw, 10, 10, 350, 350);
+            Draw(canvas, new RectF(10, 10, 350, 350));
+            image.Drawable = this;
+    }
 
-            if (photo != null)
-            {
-
-                var stream = await photo.OpenReadAsync();
-
-                var img = PlatformImage.FromStream(stream);
-
-                if (img != null)
-                {
-                    imageDraw = img;
-                    image.Drawable = this;
-                }
-            }
-        }
+    void pickPhoto_Clicked(System.Object sender, System.EventArgs e)
+    {
+        PickPhoto();
     }
 }
 
