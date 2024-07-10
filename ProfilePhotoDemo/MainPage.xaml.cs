7b9ca7953d33d021963978ed079b27e19748f201
Sweekriti Satpathy <swsat@microsoft.com>
2024-07-10 12:08:10 -0700

    adds sensitivity to gestures

--- ProfilePhotoDemo/MainPage.xaml.cs
+++ ProfilePhotoDemo/MainPage.xaml.cs
@@ -10,8 +10,11 @@ public partial class MainPage : ContentPage
     private float _rotationAngle = 0;
     private float _scale = 1;
     private float _startScale = 1;
-    private Point _startPoint;
+    private Point _startPanTranslation;
     private Point _currentTranslation;
+    private bool _isPinching = false;
+    private readonly float _panSensitivity = 0.5f; // Adjust this value to control pan sensitivity
+    private readonly float _pinchSensitivity = 0.9f; // Adjust this value to control pinch sensitivity
 
     public MainPage()
 	{
@@ -58,31 +61,74 @@ public partial class MainPage : ContentPage
 
     private void OnPinchUpdated(object sender, PinchGestureUpdatedEventArgs e)
     {
+        // if (e.Status == GestureStatus.Started)
+        //     {
+        //         // Store the initial scale factor
+        //         _startScale = _scale;
+        //         _isPinching = true;
+        //     }
+        //     else if (e.Status == GestureStatus.Running)
+        //     {
+        //         // Calculate the scale factor
+        //         _scale = _startScale * (float)e.Scale;
+        //         image.Drawable = new ImageDrawable(_image, _rotationAngle, _scale, _currentTranslation);
+        //         image.Invalidate();
+        //     }
+        //     else if (e.Status == GestureStatus.Completed || e.Status == GestureStatus.Canceled)
+        //     {
+        //         _isPinching = false;
+        //     }
+
         if (e.Status == GestureStatus.Started)
-        {
-            // Store the initial scale factor
-            _startScale = _scale;
-        }
-        else if (e.Status == GestureStatus.Running)
-        {
-            // Calculate the scale factor
-            _scale = (float)(_startScale * e.Scale);
-            image.Drawable = new ImageDrawable(_image, _rotationAngle, _scale, _currentTranslation);
-            image.Invalidate();
-        }
+            {
+                // Store the initial scale factor
+                _startScale = _scale;
+                _isPinching = true;
+            }
+            else if (e.Status == GestureStatus.Running)
+            {
+                // Calculate the scale factor with sensitivity adjustment
+                _scale = (float)(_startScale * (1 + (e.Scale - 1) * _pinchSensitivity));
+                image.Drawable = new ImageDrawable(_image, _rotationAngle, _scale, _currentTranslation);
+                image.Invalidate();
+            }
+            else if (e.Status == GestureStatus.Completed || e.Status == GestureStatus.Canceled)
+            {
+                _isPinching = false;
+            }
     }
 
     private void OnPanUpdated(object sender, PanUpdatedEventArgs e)
     {
-        if (_image == null)
-            return;
-        if (e.StatusType == GestureStatus.Running)
-        {
-            // Update the translation point
-            _currentTranslation = new Point(_currentTranslation.X + e.TotalX, _currentTranslation.Y + e.TotalY);
-            image.Drawable = new ImageDrawable(_image, _rotationAngle, _scale, _currentTranslation);
-            image.Invalidate();
-        }
+    //    if (_image == null || _isPinching)
+    //             return;
+
+    //         if (e.StatusType == GestureStatus.Running)
+    //         {
+    //             // Update the translation point
+    //             _currentTranslation = new Point(_currentTranslation.X + e.TotalX, _currentTranslation.Y + e.TotalY);
+    //             image.Drawable = new ImageDrawable(_image, _rotationAngle, _scale, _currentTranslation);
+    //             image.Invalidate();
+    //         }
+
+            if (_image == null || _isPinching)
+                return;
+
+            if (e.StatusType == GestureStatus.Started)
+            {
+                // Store the initial translation point
+                _startPanTranslation = _currentTranslation;
+            }
+            else if (e.StatusType == GestureStatus.Running)
+            {
+                // Update the translation point based on the pan gesture with sensitivity adjustment
+                _currentTranslation = new Point(
+                    _startPanTranslation.X + e.TotalX * _panSensitivity,
+                    _startPanTranslation.Y + e.TotalY * _panSensitivity
+                );
+                image.Drawable = new ImageDrawable(_image, _rotationAngle, _scale, _currentTranslation);
+                image.Invalidate();
+            }
     }
 }
 
