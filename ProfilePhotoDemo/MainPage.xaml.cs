5300a412529e555267b5b2e9ddd9a28e5ed941d3
Sweekriti Satpathy <swsat@microsoft.com>
2024-07-10 11:09:36 -0700

    rotating works?

--- ProfilePhotoDemo/MainPage.xaml.cs
+++ ProfilePhotoDemo/MainPage.xaml.cs
@@ -1,22 +1,16 @@
 ﻿using Microsoft.Maui.Graphics.Platform;
 using IImage = Microsoft.Maui.Graphics.IImage;
 
-namespace ProfilePhotoDemo;
-
-public partial class MainPage : ContentPage, IDrawable
+namespace ProfilePhotoDemo
 {
-    public IImage imageDraw { get; set; }
+
+public partial class MainPage : ContentPage
+{
+     private IImage _image;
+    private float _rotationAngle = 0;
     public MainPage()
 	{
         InitializeComponent();
-    }
-
-    public void Draw(ICanvas canvas, RectF dirtyRect)
-    {
-        if (imageDraw != null)
-        {
-            canvas.DrawImage(imageDraw, 10, 10, 350, 350);
-        }
     }
 
     public async void PickPhoto()
@@ -28,12 +22,13 @@ public partial class MainPage : ContentPage, IDrawable
             {
                 var stream = await photo.OpenReadAsync();
 
-                var img = PlatformImage.FromStream(stream);
+                _image = PlatformImage.FromStream(stream);
+                _rotationAngle = 0; // Reset rotation angle
 
-                if (img != null)
+                if (_image != null)
                 {
-                    imageDraw = img;
-                    image.Drawable = this;
+                    image.Drawable = new ImageDrawable(_image, _rotationAngle);
+                    image.Invalidate();
                 }
             }
         }
@@ -41,16 +36,23 @@ public partial class MainPage : ContentPage, IDrawable
 
     void rotatePhotoClock_Clicked(System.Object sender, System.EventArgs e)
     {
-            var service = new PlatformBitmapExportService();
-            var context = service.CreateContext(350, 350);
-            var canvas = context.Canvas;
+            // var service = new PlatformBitmapExportService();
+            // var context = service.CreateContext(350, 350);
+            // var canvas = context.Canvas;
 
-            // draw using canvas
-            var imageRotate = context.Image;
-            canvas.Rotate(90);
-            // canvas.DrawImage(imageDraw, 10, 10, 350, 350);
-            Draw(canvas, new RectF(10, 10, 350, 350));
-            image.Drawable = this;
+            // // draw using canvas
+            // var imageRotate = context.Image;
+            // canvas.Rotate(90);
+            // // canvas.DrawImage(imageDraw, 10, 10, 350, 350);
+            // Draw(canvas, new RectF(10, 10, 350, 350));
+            // image.Drawable = this;
+
+            if (_image != null)
+            {
+                _rotationAngle = (_rotationAngle + 90) % 360;
+                image.Drawable = new ImageDrawable(_image, _rotationAngle);
+                image.Invalidate();
+            }
     }
 
     void pickPhoto_Clicked(System.Object sender, System.EventArgs e)
@@ -59,4 +61,31 @@ public partial class MainPage : ContentPage, IDrawable
     }
 }
 
+    public class ImageDrawable : IDrawable
+    {
+        private readonly IImage _image;
+        private readonly float _rotationAngle;
 
+        public ImageDrawable(IImage image, float rotationAngle)
+        {
+            _image = image;
+            _rotationAngle = rotationAngle;
+        }
+
+        public void Draw(ICanvas canvas, RectF dirtyRect)
+        {
+            if (_image == null)
+                return;
+
+            var width = 350;
+            var height = 350;
+
+            // Apply rotation transformation
+            canvas.SaveState();
+            canvas.Rotate(_rotationAngle, width / 2, height / 2);
+            canvas.DrawImage(_image, 0, 0, width, height);
+            canvas.RestoreState();
+        }
+    }
+
+}
